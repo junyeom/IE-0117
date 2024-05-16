66d854dff3818b7dffb3606cd1f7a696b27f01a6
junyeom <jhyeomsong@gmail.com>
2024-05-15 21:23:53 -0600

    Agregados los códigos del ejercicio 2

--- laboratorio4/ej2B.c
+++ laboratorio4/ej2B.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
+/*
+La función findMinMax recibe cuatro parámetros: el arreglo,
+el puntero del valor mínimo, el puntero del valor máximo
+y el tamaño del arreglo.
+
+Inicialmente, el puntero del valor mínimo apunta hacia el
+primer elemento del arreglo, y el puntero del valor máximo
+apunta hacia el último elemento del arreglo.
+
+Utilizando un ciclo for, se desreferencia el valor guardado
+en el puntero del valor mínimo, y se va sustituyendo por
+valores menores que el anterior hasta encontrar el menor
+valor. Se sigue una lógica similar para encontrar el mayor valor.
+
+Nótese que en ningún momento es necesario guardar el valor
+en una variable, porque se trabaja directamente con los punteros.
+Además, los punteros permiten que una función "retorne" varios valores
+a la vez, aunque dicha función sea de tipo void.
+*/
+
 void findMinMax (int array[], int *minimum, int *maximum, int size){
     int i;
     *minimum = array[0];
@@ -19,18 +39,23 @@ void findMinMax (int array[], int *minimum, int *maximum, int size){
 
 int main (){
 
+    // Inicialización de variables
+
     int i;
     int array[] = {21, 24, 65, 3, 56, 12, 25, 15};
     int tamanoArreglo = sizeof(array)/sizeof(array[0]);
-    printf ("Tamano del arreglo: %d\n", tamanoArreglo);
+    int minimum, maximum;
+
+    // Impresión del arreglo
     
-    printf("Arreglo original: ");
+    printf("Arreglo: ");
     for (i=0; i<tamanoArreglo; i++){
         printf("%d ", array[i]);
     }
     printf("\n");
 
-    int minimum, maximum;
+    // Ejecución de la función
+
     findMinMax(array, &minimum, &maximum, tamanoArreglo);
 
     printf ("El valor minimo es %d, y el valor maximo es %d\n", minimum, maximum);
